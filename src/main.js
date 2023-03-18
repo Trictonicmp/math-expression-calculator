db087873f9293569f47ac92d8436db4c626a7956
Trictonicmp <code_shadow@hotmail.com>
2023-03-17 20:55:43 -0600

    Add postfix parser main file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { tokenize } from "./tokenizer.js";
 import { infixToPostfix } from "./postfix-conversor.js";
+import { postfixParser } from "./psotfix-parser.js";
 
 // simple math expresion calculator
 // - tokenize expression
@@ -8,5 +9,5 @@ import { infixToPostfix } from "./postfix-conversor.js";
 // - expression evaluation with shunting yard algorithm
 
 const tokens = tokenize("2   + 5  /   (  3 * 14 . 16 ) ");
-console.log(tokens);
-console.log(infixToPostfix(tokens));
+const postfixTokens = infixToPostfix(tokens);
+console.log(postfixParser(postfixTokens));
